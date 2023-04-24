41b83f4334b5400eff35d52df5fb129a4be92f76
Alexsander <alexsandercaac@gmail.com>
2023-04-24 20:44:48 -0300

    Added abstract class to univariate models

--- industryts/models/univariate.py
+++ industryts/models/univariate.py
@@ -2,19 +2,38 @@
     Module with classes for univariate models.
 """
 import abc
+
 import numpy as np
 import pandas as pd
+
+from typing import Union
 
 
 class UnivariateModel(metaclass=abc.ABCMeta):
     """
     Base class for univariate models.
     """
+
     def __init__(self):
         super(UnivariateModel, self).__init__()
 
-    def fit(self, data: pd.DataFrame, **kwargs):
+    @abc.abstractmethod
+    def fit(self, data: Union[pd.DataFrame, np.ndarray], **kwargs):
         """
         Fit the model to the data.
         """
         pass
+
+    @abc.abstractmethod
+    def predict(self, data: Union[pd.DataFrame, np.ndarray], **kwargs):
+        """
+        Predict the values of the data.
+        """
+        raise NotImplementedError()
+
+    @abc.abstractmethod
+    def __call__(
+            self,
+            data: Union[pd.DataFrame, np.ndarray],
+            **kwargs) -> np.ndarray:
+        self.predict(data, **kwargs)
