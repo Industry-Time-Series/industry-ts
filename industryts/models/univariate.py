01ef31eb95aef76a6b9bbb4108232d3f95d367a0
Alexsander <alexsandercaac@gmail.com>
2023-04-26 14:36:17 -0300

    Started development of AutoRegressive (AR) model

--- industryts/models/univariate.py
+++ industryts/models/univariate.py
@@ -37,3 +37,46 @@ class UnivariateModel(metaclass=abc.ABCMeta):
             data: Union[pd.DataFrame, np.ndarray],
             **kwargs) -> np.ndarray:
         self.predict(data, **kwargs)
+
+
+class AutoRegressive(UnivariateModel):
+    """
+    Class for purely autoregressive models of order p.
+
+    Args:
+        p (int): Order of the model. Defaults to 1.
+        bias (bool): Whether to include a bias term in the model.
+            Defaults to True.
+
+    Attributes:
+        p: Order of the model.
+        coef: Coefficients of the model.
+    """
+
+    def __init__(self, p: int = 1, bias: bool = True):
+        super(AutoRegressive, self).__init__()
+        self.p = p
+        self.coef = None
+        self.__bias = bias
+
+    def fit(self, data: Union[pd.DataFrame, np.ndarray], **kwargs):
+        """
+        Fit the model to the data.
+        """
+        if isinstance(data, pd.DataFrame):
+            data = data.values
+        if self.__bias:
+            data = np.hstack([np.ones((data.shape[0], 1)), data])
+        regressors = np.vstack([data[i:-self.p + i] for i in range(self.p)])
+        targets = data[self.p:]
+        self.coef = np.linalg.lstsq(regressors, targets, rcond=None)[0]
+
+    def predict(self, data: Union[pd.DataFrame, np.ndarray], **kwargs):
+        """
+        Predict the values of the data.
+        """
+        if isinstance(data, pd.DataFrame):
+            data = data.values
+        if self.__bias:
+            data = np.hstack([np.ones((data.shape[0], 1)), data])
+        return data @ self.coef
