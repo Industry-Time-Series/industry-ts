4ea503b183517c51175f8dbad2e635361919398b
Alexsander <alexsandercaac@gmail.com>
2023-04-28 18:56:57 -0300

    Added moving average model class

--- industryts/models/univariate.py
+++ industryts/models/univariate.py
@@ -24,12 +24,15 @@ class UnivariateModel(metaclass=abc.ABCMeta):
         """
         pass
 
-    @abc.abstractmethod
-    def predict(self, data: Union[pd.DataFrame, np.ndarray], **kwargs):
+    def predict(self, data: Union[pd.DataFrame, np.ndarray]):
         """
         Predict the values of the data.
         """
-        raise NotImplementedError()
+        if isinstance(data, pd.DataFrame):
+            data = data.values
+        if self.__bias:
+            data = np.hstack([np.ones((data.shape[0], 1)), data])
+        return data @ self.coef
 
     def __call__(
             self,
@@ -71,15 +74,58 @@ class AutoRegressive(UnivariateModel):
             [data[i:-self.p + i] for i in range(self.p)])
         targets = data[self.p:]
         if self.__bias:
-            regressors = np.hstack([np.ones((regressors.shape[0], 1)), regressors])
+            regressors = np.hstack(
+                [np.ones((regressors.shape[0], 1)), regressors])
         self.coef = np.linalg.lstsq(regressors, targets, rcond=None)[0]
 
-    def predict(self, data: Union[pd.DataFrame, np.ndarray], **kwargs):
+
+class MovingAverage(UnivariateModel):
+    """
+    Class for purely moving average models of order q.
+
+    Args:
+        q (int): Order of the model. Defaults to 1.
+        bias (bool): Whether to include a bias term in the model.
+            Defaults to True.
+
+    Attributes:
+        q: Order of the model.
+        coef: Coefficients of the model.
+    """
+
+    def __init__(self, q: int = 1, bias: bool = True):
+        super(MovingAverage, self).__init__()
+        self.q = q
+        self.coef = None
+        self.__bias = bias
+
+    def fit(self, data: Union[pd.DataFrame, np.ndarray],
+            n_iterations: int = 100):
         """
-        Predict the values of the data.
+        Fit the model to the data.
         """
         if isinstance(data, pd.DataFrame):
             data = data.values
-        if self.__bias:
-            data = np.hstack([np.ones((data.shape[0], 1)), data])
-        return data @ self.coef
+        # If data is 1D, make it 2D
+        if data.ndim == 1:
+            data = data.reshape(-1, 1)
+        self.coef = [0 for _ in range(self.q)]
+        # Initialize the residuals
+        delayed_residuals = np.hstack(
+            [data[i:-self.q + i] for i in range(self.q)])
+        targets = data[self.q:]
+        for _ in range(n_iterations):
+            # Calculate the coefficients
+            if self.__bias:
+                regressors = np.hstack(
+                    [np.ones((delayed_residuals.shape[0], 1)),
+                     delayed_residuals])
+            else:
+                regressors = delayed_residuals
+            self.coef = np.linalg.lstsq(regressors, targets, rcond=None)[0]
+            # Calculate the residuals
+            residuals = targets - regressors @ self.coef
+            # Update the delayed residuals
+            delayed_residuals = np.zeros((residuals.shape[0], self.q))
+            for i in range(self.q):
+                delayed_residuals[i + 1:, i] = residuals[:-(i + 1)].flatten()
--- industryts/testing.py
+++ industryts/testing.py
@@ -6,22 +6,39 @@ import models.univariate as unimodels
 import matplotlib.pyplot as plt
 import numpy as np
 
-from generation.synthetic import ar_process
+from generation.synthetic import ar_process, ma_process
 
 
-N_EXPERIMENTS = 400
+N_EXPERIMENTS = 500
 TRUE_COEFS = [0.4]
-SAMPLES = 500
+SAMPLES = 600
+PROCESS = 'MA'
+BIAS = True
 
 coefs = []
+if BIAS:
+    biases = []
 
 for _ in range(N_EXPERIMENTS):
-    data = ar_process(TRUE_COEFS, samples=SAMPLES, noise=0)
-    ar_model = unimodels.AutoRegressive(p=1, bias=False)
-    ar_model.fit(data)
-    coefs.append(ar_model.coef)
+    if PROCESS == 'AR':
+        data = ar_process(TRUE_COEFS, samples=SAMPLES, noise=0)
+        ar_model = unimodels.AutoRegressive(p=1, bias=BIAS)
+        ar_model.fit(data)
+        if BIAS:
+            biases.append(ar_model.coef[0])
+        coefs.append(ar_model.coef[1:])
+    elif PROCESS == 'MA':
+        data = ma_process(TRUE_COEFS, samples=SAMPLES, noise=0)
+        ma_model = unimodels.MovingAverage(q=1, bias=BIAS)
+        ma_model.fit(data)
+        if BIAS:
+            biases.append(ma_model.coef[0])
+        coefs.append(ma_model.coef[1:])
+    else:
+        raise ValueError("Invalid process type.")
 
 coefs = np.array(coefs).squeeze()
+
 plt.hist(coefs, bins=100)
 
 plt.axvline(x=TRUE_COEFS[0], color='red')
@@ -34,5 +51,16 @@ plt.ylabel("Frequency")
 plt.show()
 
 
-print("Mean: ", np.mean(coefs))
+print("Mean of coefficient estimates: ", np.mean(coefs))
 print("Standard deviation: ", np.std(coefs))
+
+if BIAS:
+    print("Mean of bias estimates: ", np.mean(biases))
+    print("Standard deviation: ", np.std(biases))
+
+# Plot the data
+plt.plot(data)
+plt.title(PROCESS + f"({len(TRUE_COEFS)}) process")
+plt.xlabel("Time")
+plt.ylabel("Value")
+plt.show()
