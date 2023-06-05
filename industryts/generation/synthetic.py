01aeaf3c0589fb4621c09f87d40353896b374b1e
Alexsander <alexsandercaac@gmail.com>
2023-06-05 15:34:58 -0300

    Reuploaded synthetic functions lost in merge

--- industryts/generation/synthetic.py
+++ industryts/generation/synthetic.py
@@ -161,3 +161,90 @@ def trend_component(samples: int = 100, slope: float = 0.1,
         y += np.random.normal(0, noise, samples)
 
     return np.array(y)
+
+
+def discontinuous_timeseries(start_date: Union[str, pd.Timestamp],
+                             end_date: Union[str, pd.Timestamp],
+                             freq: Union[str, pd.Timedelta],
+                             num_discontinuities: int,
+                             is_categorical: bool = False) -> pd.Series:
+    """
+    Generate a discontinuous time series with random data.
+
+    Args:
+        start_date (str or Timestamp): start date of the time series.
+        end_date (str or Timestamp): end date of the time series.
+        freq (str or Timedelta): frequency of the time series.
+        num_discontinuities (int): number of discontinuity points to be
+        generated.
+
+    Returns:
+        ts (Series): discontinuous time series with random data.
+    """
+    # Create a date range with specified frequency
+    if isinstance(start_date, str):
+        start_date = pd.Timestamp(start_date)
+    if isinstance(end_date, str):
+        end_date = pd.Timestamp(end_date)
+    if isinstance(freq, str):
+        freq = pd.Timedelta(freq)
+    date_range = pd.date_range(start=start_date, end=end_date, freq=freq)
+    # Randomly select discontinuity points
+    discontinuity_points = np.random.choice(date_range, num_discontinuities,
+                                            replace=False)
+    # Create the time series with random data
+    if is_categorical:
+        data = np.random.choice(['A', 'B', 'C', 'D'], len(date_range))
+    else:
+        data = np.random.rand(len(date_range))
+    ts = pd.Series(data, index=date_range)
+    # Drop NaN values at the discontinuity points
+    for point in discontinuity_points:
+        ts.drop(point, inplace=True)
+    return ts
+
+
+def part_static_timeseries(start_date: Union[str, pd.Timestamp],
+                           end_date: Union[str, pd.Timestamp],
+                           frequency: Union[str, pd.Timedelta],
+                           n_samples_static: int,
+                           value_static: float = 1.0) -> pd.DataFrame:
+    """Generate a time series with a part that is static.
+
+    Args:
+        start_date (Union[str, pd.Timestamp]): Start date of the time series.
+        end_date (Union[str, pd.Timestamp]): End date of the time series.
+        n_samples_static (int): Number of samples that will be static.
+
+    Raises:
+        ValueError: Error raised if the number of samples is less than the
+        number of static samples.
+
+    Returns:
+        pd.DataFrame: Time series with a part that is static.
+    """
+    if isinstance(start_date, str):
+        start_date = pd.Timestamp(start_date)
+    if isinstance(end_date, str):
+        end_date = pd.Timestamp(end_date)
+    if isinstance(frequency, str):
+        frequency = pd.Timedelta(frequency)
+    # Generate a DatetimeIndex with a frequency of 1 minute
+    dt_index = pd.date_range(start=start_date, end=end_date, freq=frequency)
+
+    # Create 3 random float columns
+    col1 = np.random.randn(len(dt_index))
+    col2 = np.random.randn(len(dt_index))
+    col3 = np.random.randn(len(dt_index))
+
+    if len(dt_index) < n_samples_static:
+        raise ValueError('The number of samples must be greater than the '
+                         'number of static samples.')
+    # Set one of the columns to a constant value for n_samples_static samples
+    col1[:n_samples_static] = value_static
+
+    # Create the DataFrame
+    df = pd.DataFrame({'col1': col1, 'col2': col2, 'col3': col3},
+                      index=dt_index)
+
+    return df
